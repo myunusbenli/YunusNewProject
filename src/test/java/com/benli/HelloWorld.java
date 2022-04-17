767e374c552abce048a4ddb277599fefbff2d841
FrameworkPractices <muhammed.yunus.benli@gmail.com>
2022-04-17 15:37:50 -0500

    initial commit 4

--- src/test/java/com/benli/HelloWorld.java
+++ src/test/java/com/benli/HelloWorld.java
@@ -3,5 +3,6 @@ package com.benli;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World in the Git World");
+        System.out.println("we are doing git practice");
     }
 }
